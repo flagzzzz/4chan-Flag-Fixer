35da9f686cebe656ba017a5eb102917a8d5b9f7b
flagzzzz <hotheads4u@gmail.com>
2016-06-27 22:45:13 -0400

    Added redirecting a1, a2, and ap

--- 4chan_flag_fixer.user.js
+++ 4chan_flag_fixer.user.js
@@ -9,7 +9,7 @@
 // @exclude     http*://boards.4chan.org/int/catalog
 // @exclude     http*://boards.4chan.org/sp/catalog
 // @exclude     http*://boards.4chan.org/pol/catalog
-// @version     1.34
+// @version     1.35
 // @grant       GM_registerMenuCommand
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -94,6 +94,29 @@ function load(key) {
 	return GM_getValue(nameSpace + key);
 }
 
+function parseA1A2Posts() {
+    var elements = document.getElementsByClassName('flag-a1');
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function() { window.open(flagReplaceUrl + "flags/" + flagOption + "/a1.gif"); };
+        elements[i].title = "Anonymous Proxy";
+    }
+
+    elements = document.getElementsByClassName('flag-a2');
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function() { window.open(flagReplaceUrl + "flags/" + flagOption + "/a2.gif"); };
+        elements[i].title = "Satellite Provider";
+    }
+    
+    elements = document.getElementsByClassName('flag-ap');
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function() { window.open(flagReplaceUrl + "flags/" + flagOption + "/ap.gif"); };
+        elements[i].title = "Asia/Pacific Region";
+    }
+}
+
 
 flagOption = load(flagOptionsVar);
 if (!flagOption || flagOption === "" || flagOption === "undefined") {
@@ -105,11 +128,31 @@ GM_addStyle(".flag { display:inline-block; width:16px; height:11px; position:rel
 GM_addStyle(".flag-a1 { background-position:-32px -176px !important; }");
 GM_addStyle(".flag-a2 { background-position:-48px -176px !important; }");
 GM_addStyle(".flag-ap { background-position:-64px -176px !important; }");
+parseA1A2Posts();
 var address = window.location.href;
 if (address.indexOf("4cdn.org/image/country") > -1) {
     var parent = document.querySelector('img').parentNode;
     parent.removeChild(document.querySelector('img'));
     var img = document.createElement('img');
-    img.src = flagReplaceUrl + "flags/" + flagOption + "/" + address.substring(address.length - 6, address.length);
+    var newFlag = flagReplaceUrl + "flags/" + flagOption + "/" + address.substring(address.length - 6, address.length);
+    img.src = newFlag;
     parent.appendChild(img);
+    
+    (function() {
+        var link = document.createElement('link');
+        link.type = 'image/png';
+        link.rel = 'shortcut icon';
+        link.href = newFlag;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }());
 }
+
+//thread listeners for a1, a2, and ap assignments
+document.addEventListener('ThreadUpdate', function (e) {
+    setTimeout(parseA1A2Posts(), 0);
+}, false);
+
+document.addEventListener('4chanThreadUpdated', function (e) {
+    //setTimeout to support greasemonkey 1.x
+    setTimeout(parseA1A2Posts(), 0);
+}, false);
